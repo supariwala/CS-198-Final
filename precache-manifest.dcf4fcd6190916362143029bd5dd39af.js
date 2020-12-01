0aa27ecfec958963953e55f1df309093a6f7f225
Amaan Supariwala <amaansupariwala@berkeley.edu>
2020-11-30 21:54:32 -0500

    First commit

---
+++ precache-manifest.dcf4fcd6190916362143029bd5dd39af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43e08fe2d5855bc8f51df60d4aaa142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e781fa33d49bc34ca9b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e781fa33d49bc34ca9b",
+    "url": "/static/js/main.f2d89b36.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
